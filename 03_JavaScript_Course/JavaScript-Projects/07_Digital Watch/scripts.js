56d50231dc3fb33a234fad5213d6bfb7e3b916de
ankittripathe <ankittripathi2k24@gmail.com>
2026-04-04 18:47:34 +0530

    JavaScript_Course

--- 03_JavaScript_Course/JavaScript-Projects/07_Digital Watch/scripts.js
+++
@@ -1,8 +0,0 @@
-const showClock = document.getElementById("clock");
-
-function currentTime() {
-  const date = new Date();
-  // console.log(date.toLocaleTimeString());
-  showClock.innerHTML = date.toLocaleTimeString();
-}
-setInterval(currentTime, 1000); // AsynSchronous JavaScripts
--- 03_JavaScript_Course/JavaScript-Projects/README.md
+++
@@ -1,2 +0,0 @@
-# JavaScript-Projects
-A Repo Series of JavaScripts Projects..
