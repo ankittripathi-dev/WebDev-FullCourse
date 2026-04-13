174007e61c55d7a06f48a2cd6316da191942e115
ankittripathe <ankittripathi2k24@gmail.com>
2026-04-13 18:59:00 +0530

    Typescripts

--- 08_TypeScripts/src/tutorials.tsx
+++ 08_TypeScripts/src/tutorials.tsx
@@ -1,35 +1,34 @@
 //! (1) Number
 // Example.1
-let num1: number;
-num1 = 10;
+let num1: number = 100
 
 // Example.2
-let num2: number = 200;
+let num2: number
+num2 = 200;
 
 
 //! (2) String
 // Example.1
-let str1: string;
-str1 = "ankit";
+let str1: string = "hello dosto"
 
 // Example.2
-let str2: string = "hello dosto";
+let str2: string;
+str2 = "Good Morning";
 
 
 //! (3) boolean
 // Example.1
-let isOnline: boolean;
-isOnline = true;
+let isOnline: boolean = true;
 
 // Example.2
-let isAdult: boolean = true;
+let isAdult: boolean
+isAdult = false;
 
 
 //! (4) Array
 // Example.1
 let array1: number[];
 array1 = [11, 22];
-
 
 // Example.2
 let array2: number[] = [10, 20, 30];
@@ -38,10 +37,10 @@ let array2: number[] = [10, 20, 30];
 let array3: string[] = ["ShaktiMaan", "SuperMan", "SpiderMan"];
 
 // Example.4
-let array4: boolean[]= [true, false, true]
+let array4: boolean[] = [true, false, true];
 
 // Example.5
-let array5: object[] = [{name:"ankit"}, {role:"software Engineer"}]
+let array5: object[] = [{ name: "ankit" }, { role: "software Engineer" }];
 
 // Example.6
 let myArray1: Array<number> = [1, 2, 3];
@@ -50,22 +49,27 @@ let myArray1: Array<number> = [1, 2, 3];
 let myArray2: Array<string> = ["sunny", "hunny", "bunny"];
 
 // Example.8
-let myArray3: Array<boolean> = [true, false , true]
+let myArray3: Array<boolean> = [true, false, true];
 
 // Example.9
-let myArray4: Array<object> = [{name:"ankit", role:"software Engineer"}]
+let myArray4: Array<object> = [{ name: "ankit", role: "software Engineer" }];
 
 
 //! (5) Tuple
 // Example.1
-let arr3: [number, string, boolean];
-arr3 = [7, "dell", true];
+let arr3: [number, string] = [100, "ankit"];
 
 // Example.2
-let arr4: [number, string] = [100, "ankit"];
+let arr4: [number, string, boolean];
+arr4 = [7, "dell", true];
 
 // Example.3
-let arr5: [number, object, boolean, string] = [1, {name:"mahi"}, true, "ankit"]
+let arr5: [number, object, boolean, string] = [
+  1,
+  { name: "mahi" },
+  true,
+  "ankit",
+];
 
 
 //! 6 Enum :- Enum (Enumeration) is used to define a set of named constants
@@ -97,20 +101,33 @@ enum CurrentStatus {
   LOADING = "loading",
 }
 
+
 //! (7) Any
 // Example.1
-let c: any;
-
-c = 78;
-c = "ankit";
-c = false;
-c = ["ankit"];
+let c: any = 11
 
 // Example.2
+let d: any = "lovely"
+
+// Example.3
+let e: any = true
+
+// Example.4
+let f: any = ["money", "honey"]
+
+// Exampe.5
+let g : any;
+g = 78;
+g = "ankit";
+g = false;
+g = ["ankit"];
+
+
+// Example.6
 let some: any = ["ram", 12, false, ["urvil", "ayush", "mahi"]];
 
 
-//! (8 Function in TypeScript
+//! (8) Function
 //! (a) basic function :- TypeScript lets you define types for parameters and return value.
 // Example.1
 function greet(name: string): string {
@@ -127,31 +144,22 @@ function hello(name: string, age: number): string {
 }
 hello("ankit", 26);
 // name: string --> parameter type
-// age:number --> parameter type
+// age: number --> parameter type
 // : string --> return type
 
 
-//! (b) Function Without Return (void) :- void means nothing
-// Example.1
-function logMessage(msg: string): void {
-  console.log(msg);
-}
-logMessage("hello");
-// void means the function does not return anything
-
-
-// Example.2
+// Example.3
 function ayush(a: number, b: string) {
   return false;
 }
-// TypeScript automatically detects return type so, here return type is boolean
+// ✅ TypeScript automatically detects return type so, Here return type is boolean
 
 
-// Example.3
+// Example.4
 function add(a: number, b: number) {
   return a + b;
 }
-// ✅ TypeScript automatically detects return type So, here return type is number
+// ✅ TypeScript automatically detects return type So, Here return type is number
 
 /*
 ✅ Your Code
@@ -166,12 +174,18 @@ function add(a: number, b: number) {
 - b is number
 - a + b → result is number
  So TypeScript understands:
-
-// Inferred by TypeScript
-function add(a: number, b: number): number {
-  return a+b // number
 }
 */
+
+
+//! (b) Function Without Return (void):- void means nothing
+// Example.1
+function logMessage(msg: string): void {
+  console.log(msg);
+}
+logMessage("hello");
+// void means the function does not return anything
+
 
 //! (c) Default Parameters
 // Example.1
@@ -181,10 +195,10 @@ function greetDefault(name: string = "Guest"): string {
 greetDefault(); // Hello Guest
 
 // Example.2
-function sayHello(a: number, b: string = "moon"): number {
+function sayHello(a: number, b: string = "onlyOne"): number {
   return 55;
 }
-sayHello(10);
+sayHello(10); 
 
 
 //! (d) Optional Parameters
@@ -192,7 +206,7 @@ sayHello(10);
 function greetUser(name: string, age?: number): string {
   return `Hello ${name}, Age: ${age ?? "Not provided"}`;
 }
-greetUser("Ankit");
+greetUser("Ankit"); 
 // 👉 ? makes parameter optional
 
 
@@ -202,16 +216,15 @@ const addition = (a: number, b: number): number => {
   return a + b;
 };
 
-
 // Example.2
-const myAddition = (a:object, b?:object): object =>{
-  return a 
-}
-myAddition({name:"Uma"})
+const myAddition = (a: object, b?: object): object => {
+  return a;
+};
+myAddition({ name: "SomeOne" });
 
 
 
-//! (9) Type Inference & Tpe Annotation
+//! (9) Type Inference & Tpe Annotation // 
 
 //! (a) Type Inference:- (Automatic) 👉 TypeScript automatically guesses the type
 // Example.1
@@ -221,12 +234,10 @@ let age1 = 22;        // Inferred as number
 
 // Example.2
 function adding(a: number, b: number) {
-  return a + b;   // inferred return type = number
+  return a + b;    // inferred return type = number
 }
-// No need to write : number
 
-
-//! (b) Type Annotation:- (Manual)  👉 You explicitly define the type
+//! (b) Type Annotation:- (Manual) 👉 You explicitly define the type
 // Example.1
 let name2: string = "Ankit";
 let age2: number = 22;
@@ -243,22 +254,28 @@ function addNum(a: number, b: number): number {
 */
 
 //!*****************************************************************************************************/
-//! (10) Type Aliases:- Type Alias lets you create a custom name for any type (primitive, object, union, function, etc).
+
+//! (10) Type Aliases:- Type Alias lets you create a custom name for any type (primitive, object, union, function, etc)
 //! (a) Basic Syntax
+// Example.1
 type User = {
   name: string;
   age: number;
 };
 // Now You Can Reuse User Anywhere
 
-const user1: User = {
+const userOne: User = {
   name: "Ankit",
   age: 22,
 };
 
+const userTwo: User = {
+  name:"Etti",
+  age:24,
+}
 
-//! (b) Object Types
-// Example.1
+
+// Example.2
 type Product = {
   id: number;
   title: string;
@@ -271,7 +288,8 @@ const item: Product = {
   price: 50000,
 };
 
-// Example.2
+
+// Example.3
 type Post = {
   description: string;
   image?: string; // optional property
@@ -279,25 +297,24 @@ type Post = {
 };
 
 let obj: Post = {
-  description: "my first post",
+  description: "My First Post",
   // image: "/image.png",
   likes: 10,
 };
-
 
 //! (c) Union Types
 // Example.1
 type ID = number | string;
 
-let userId: ID;
-userId = 101;
-userId = "abc123";
+let userId;
+userId = 7;
+userId = "Coffee";
 
 // Example.2
 type myName = number | string;
 
-// let result: myName = 100;   
-let result: myName = "Dhonee"
+var result: myName = 100;
+var result: myName = "ShaktiMaan";
 
 
 //! (d) Literal Types
@@ -321,23 +338,23 @@ const multiply: MathFun = (a, b) => {
   return a * b;
 };
 
-
 //! (f) Intersection Types:- Combine Multiple Types
 // Example.1
 type A = {
-   name: string;
+  name: string;
 };
 
 type B = {
-   age: number
+  age: number;
 };
 
 type AB = A & B;
 
-const myObj: AB= {
+const detailsOne: AB = {
   name: "Ankit",
   age: 22,
 };
+
 
 // *******************************************************************************************************
 //! (11) InterFaces:- Interface is used to define the structure (shape) of an object.
@@ -345,8 +362,8 @@ const myObj: AB= {
 interface UserDetails {
   name: string;
   age: number;
-  email?: string;   // optional
-  readonly apiKey: string;         
+  email?: string; // optional
+  readonly apiKey: string;
 }
 
 const userAnkit: UserDetails = {
@@ -355,17 +372,17 @@ const userAnkit: UserDetails = {
   apiKey: "123",
 };
 
-// !🔹 Readonly Properties
+//!🔹 Readonly Properties
 // Example.2
 interface Config {
   readonly apiKey: string;
 }
 
-const config: Config = {
+const MyConfig: Config = {
   apiKey: "123",
 };
 
-//! 🔹 Interface with Methods
+//!🔹 Interface with Methods
 // Example.3
 interface Person {
   name: string;
@@ -379,8 +396,7 @@ const p: Person = {
   },
 };
 
-
-//! 🔹 Function in Interface
+//!🔹 Function in Interface
 // Example.4
 interface AddFunction {
   (a: number, b: number): number;
@@ -388,41 +404,36 @@ interface AddFunction {
 
 const MyAddition: AddFunction = (a, b) => a + b;
 
-
 //! Using extends:- One interface inherits another
 // Example.1
 interface demoUser {
-  name:string
+  name: string;
 }
 
 interface test extends demoUser {
   age: number;
 }
 
-
 let finalResult: test = {
-  name:"Ankit",
-  age:26
-}
-
+  name: "Ankit",
+  age: 26,
+};
 
 // Example.2
 interface PersonDetails {
-  name:String;
-  age:number;
+  name: String;
+  age: number;
 }
 
 interface AdminDetails extends PersonDetails {
-  isOnline:boolean;
+  isOnline: boolean;
 }
-
 
 let adminDetails: AdminDetails = {
-  name:"Ankit",
-  age:26,
-  isOnline: true
-}
-
+  name: "Ankit",
+  age: 26,
+  isOnline: true,
+};
 
 // Diffrence Between Type & Interface In TypeScripts
 // Type
@@ -434,3 +445,16 @@ type UserDetail = {
 interface UserDetails {
   name: string;
 }
+
+// ******************************************************************************************************************** //
+
+//  Generics:- Generics in TypeScript let you write reusable, type-safe code that works with multiple data types—without losing type information.
+// Generics allow you to create functions, classes, and interfaces that work with any type while still enforcing type safety.
+
+// Basic Syntax
+function helloBoloJee<T>(a: T, b: T): T {
+  return a
+}
+helloBoloJee(10, 12);
+
+
