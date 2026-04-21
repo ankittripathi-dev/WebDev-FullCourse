a96a014fafb8572867ae0762661ebed44678dbcf
Ankit Tripathi <ankittripathi2k24@gmail.com>
2026-04-21 14:59:02 +0530

    use client

--- 09_Nextjs_Course/03_NextJs-Sheriyans/02_useclient/app/page.js
+++ 09_Nextjs_Course/03_NextJs-Sheriyans/02_useclient/app/page.js
@@ -3,12 +3,13 @@ import { useState } from "react";
 
 const Home = () => {
   const [num, setNum] = useState(0);
+  console.log("num:", num);
   
   return (
-    <div className="text-center">
-      <h2>Value of Num:{num}</h2>
+    <div className="text-center text-2xl mt-5">
+      <h1>Value of Num: {num}</h1>
       <button
-        className="bg-red-600 p-1 rounded-md"
+        className="bg-red-600 p-1 rounded-md text-2xl mt-2"
         onClick={() => setNum(num + 1)}
       >
         Increment
